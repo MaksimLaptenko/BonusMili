1101ab3df90ca93b2d28c6c34c5db00618c72600
Maksim Laptenko <laptenko0811@icloud.com>
2020-06-14 16:30:44 +0300

    First commmit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 3795;
+        int mili = price / 20;
+        System.out.println("You got " + mili + " bonus miles");
+    }
+}
